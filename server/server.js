6c8f01378928a87fa26635b412c2896043c47cb5
evgenytryzo <g0022@inbox.ru>
2023-10-23 00:08:44 +0300

    Fixed tab issues, removed dockerfile's, improved nginx.conf

--- server/server.js
+++ server/server.js
@@ -8,27 +8,27 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
 
 app.get('/thanks', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public', 'thanks.html'))
+  res.sendFile(path.join(__dirname, 'public', 'thanks.html'))
 })
 
 app.post('/submit', (req, res) => {
-	const dataObject = req.body
-	const dataString = `{\n name: ${dataObject.name},\n telegram: ${dataObject.telegram},\n email: ${dataObject.email}\n}\n`
+  const dataObject = req.body
+  const dataString = `{\n name: ${dataObject.name},\n telegram: ${dataObject.telegram},\n email: ${dataObject.email}\n}\n`
 
-	fs.appendFile(path.join(__dirname, '/data.txt'), dataString, err => {
-		if (err) {
-			console.error("Couldn't write to file: ", err)
-			return res.status(500).send('Server Error')
-		}
-		res.redirect('/thanks')
-	})
+  fs.appendFile(path.join(__dirname, '/data.txt'), dataString, err => {
+    if (err) {
+      console.error("Couldn't write to file: ", err)
+      return res.status(500).send('Server Error')
+    }
+    res.redirect('/thanks')
+  })
 })
 
 app.get('/download', function (req, res) {
-	const filePath = path.join(__dirname, '/data.txt')
-	res.download(filePath)
+  const filePath = path.join(__dirname, '/data.txt')
+  res.download(filePath)
 })
 
 app.listen(3000, () => {
-	console.log('App listening on port 3000')
+  console.log('App listening on port 3000')
 })
